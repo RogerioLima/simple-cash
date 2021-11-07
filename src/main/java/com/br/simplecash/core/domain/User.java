ce9bdfc06e01a69c1ab0dd2b9fd6baa4e03f5f0c
rogerio <rogerio.nunes@luizalabs.com>
2021-11-07 01:02:07 -0300

    implemented controller of endpoint create user

---
+++ src/main/java/com/br/simplecash/core/domain/User.java
@@ -0,0 +1,20 @@
+package com.br.simplecash.core.domain;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+	private Long userCode;
+	private String name;
+	private String email;
+	private LocalDate birthDate;
+	private String password;
+}
